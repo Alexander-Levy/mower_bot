1f31dee851fd741352c8b32339f30ac7c22f6999
Alexander Levy <edward7030425@gmail.com>
2024-10-29 23:05:07 -0400

    update rplidar launch parameters

--- rplidar_ros2/launch/rplidar.launch.py
+++ rplidar_ros2/launch/rplidar.launch.py
@@ -10,10 +10,9 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/ttyUSB0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
                 'serial_baudrate': 115200,  # A1 / A2
-                # 'serial_baudrate': 256000, # A3
-                'frame_id': 'laser',
+                'frame_id': 'laser_frame',
                 'inverted': False,
                 'angle_compensate': True,
             }],
