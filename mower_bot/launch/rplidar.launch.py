5420124365493b72473161c371bd74942d2f6cd7
Alexander Levy <edward7030425@gmail.com>
2024-10-29 23:39:07 -0400

    updated rplidar launch file

--- mower_bot/launch/rplidar.launch.py
+++ mower_bot/launch/rplidar.launch.py
@@ -1,20 +1,20 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
     return LaunchDescription([
-
         Node(
+            name='rplidar_composition',
             package='rplidar_ros',
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'serial_baudrate': 115200,  # A1 / A2
                 'frame_id': 'laser_frame',
+                'inverted': False,
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
-            }]
-        )
+            }],
+        ),
     ])
