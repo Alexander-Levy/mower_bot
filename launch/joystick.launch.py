2097c9f86c6e3dc8cbbb65537abaa43585e3dd34
Alexander Levy <edward7030425@gmail.com>
2024-11-12 05:33:08 -0400

    Cleaned up code

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -5,6 +5,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 
+
 def generate_launch_description():
 
     # Package name
@@ -12,6 +13,11 @@ def generate_launch_description():
 
     # Launch configuration
     use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Launch Arguments
+    declare_use_sim_time = DeclareLaunchArgument(
+        name='use_sim_time', default_value='false',
+        description='Use simulation(Gazebo) clock if true')
 
     # Get the params file for our joystick
     joy_params = os.path.join(package_name,'config','joystick.yaml')
@@ -32,11 +38,9 @@ def generate_launch_description():
             remappings=[('/cmd_vel','/cmd_vel_joy')]
          )
 
+    # Launch!
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
+        declare_use_sim_time,
         joy_node,
         teleop_node       
     ])
